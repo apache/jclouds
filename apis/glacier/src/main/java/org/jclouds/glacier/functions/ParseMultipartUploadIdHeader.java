eb87d61c7b339218ba90ef21262f1e2dd6f43bd2
Andrew Gaul <gaul@apache.org>
2014-08-09 22:51:08 -0700

    Correct license headers

--- apis/glacier/src/main/java/org/jclouds/glacier/functions/ParseMultipartUploadIdHeader.java
+++ apis/glacier/src/main/java/org/jclouds/glacier/functions/ParseMultipartUploadIdHeader.java
@@ -1,9 +1,7 @@
 /*
- * Licensed to the Apache Software Foimport org.jclouds.http.HttpException;
-import org.jclouds.http.HttpResponse;
-
-import com.google.common.base.Function;
- for additional information regarding copyright ownership.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
