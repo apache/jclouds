738a01dda604433eba587869457f725b46b567d2
SATYANAN-ANAND <satyanan.anand@gmail.com>
2022-02-01 06:26:48 -0800

    Update AzureComputeApi.java

--- providers/azurecompute-arm/src/main/java/org/jclouds/azurecompute/arm/AzureComputeApi.java
+++ providers/azurecompute-arm/src/main/java/org/jclouds/azurecompute/arm/AzureComputeApi.java
@@ -55,284 +55,247 @@ import com.google.common.base.Supplier;
 import com.google.inject.Provides;
 
 /**
- * The Azure Resource Manager API is a REST API for managing your services and
- * deployments.
+ * The Azure Resource Manager API is a REST API for managing your services and deployments.
  * <p>
  *
- * @see <a href="https://msdn.microsoft.com/en-us/library/azure/dn790568.aspx"
- *      >doc</a>
+ * @see <a href="https://msdn.microsoft.com/en-us/library/azure/dn790568.aspx" >doc</a>
  */
 public interface AzureComputeApi extends Closeable {
+   
+   /**
+    * The Azure Resource Manager API includes operations for managing resource groups in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/dn790546.aspx">docs</a>
+    */
+   @Delegate
+   ResourceGroupApi getResourceGroupApi();
 
-	/**
-	 * The Azure Resource Manager API includes operations for managing resource
-	 * groups in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/dn790546.aspx">docs</a>
-	 */
-	@Delegate
-	ResourceGroupApi getResourceGroupApi();
+   /**
+    * Provides access to the Job tracking API.
+    */
+   @Delegate
+   JobApi getJobApi();
 
-	/**
-	 * Provides access to the Job tracking API.
-	 */
-	@Delegate
-	JobApi getJobApi();
+   /**
+    * This Azure Resource Manager API provides all of the locations that are available for resource providers
+    *
+    * @see <a href="https://msdn.microsoft.com/en-US/library/azure/dn790540.aspx">docs</a>
+    */
+   @Delegate
+   LocationApi getLocationApi();
 
-	/**
-	 * This Azure Resource Manager API provides all of the locations that are
-	 * available for resource providers
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-US/library/azure/dn790540.aspx">docs</a>
-	 */
-	@Delegate
-	LocationApi getLocationApi();
+   /**
+    * The Azure Resource Manager API includes operations for managing the storage accounts in your subscription.
+    *
+    * @see <https://msdn.microsoft.com/en-us/library/mt163683.aspx">docs</a>
+    */
+   @Delegate
+   StorageAccountApi getStorageAccountApi(@PathParam("resourceGroup") String resourceGroup);
 
-	/**
-	 * The Azure Resource Manager API includes operations for managing the storage
-	 * accounts in your subscription.
-	 *
-	 * @see <https://msdn.microsoft.com/en-us/library/mt163683.aspx">docs</a>
-	 */
-	@Delegate
-	StorageAccountApi getStorageAccountApi(@PathParam("resourceGroup") String resourceGroup);
+   /**
+    * The Subnet API includes operations for managing the subnets in your virtual network.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163621.aspx">docs</a>
+    */
+   @Delegate
+   SubnetApi getSubnetApi(@PathParam("resourcegroup") String resourcegroup,
+                          @PathParam("virtualnetwork") String virtualnetwork);
 
-	/**
-	 * The Subnet API includes operations for managing the subnets in your virtual
-	 * network.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163621.aspx">docs</a>
-	 */
-	@Delegate
-	SubnetApi getSubnetApi(@PathParam("resourcegroup") String resourcegroup,
-			@PathParam("virtualnetwork") String virtualnetwork);
+   /**
+    * The Virtual Network API includes operations for managing the virtual networks in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163661.aspx">docs</a>
+    */
+   @Delegate
+   VirtualNetworkApi getVirtualNetworkApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The Virtual Network API includes operations for managing the virtual networks
-	 * in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163661.aspx">docs</a>
-	 */
-	@Delegate
-	VirtualNetworkApi getVirtualNetworkApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The Network Interface Card API includes operations for managing the NICs in
-	 * your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/mt163668.aspx">docs</a>
-	 */
-	@Delegate
-	NetworkInterfaceCardApi getNetworkInterfaceCardApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The Network Interface Card API includes operations for managing the NICs in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/mt163668.aspx">docs</a>
+    */
+   @Delegate
+   NetworkInterfaceCardApi getNetworkInterfaceCardApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The Public IP Address API includes operations for managing public ID
-	 * Addresses for NICs in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163638.aspx">docs</a>
-	 */
-	@Delegate
-	PublicIPAddressApi getPublicIPAddressApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The Public IP Address API includes operations for managing public ID Addresses for NICs in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163638.aspx">docs</a>
+    */
+   @Delegate
+   PublicIPAddressApi getPublicIPAddressApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The Virtual Machine API includes operations for managing the virtual machines
-	 * in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163630.aspx">docs</a>
-	 */
-	@Delegate
-	VirtualMachineApi getVirtualMachineApi(@Nullable @PathParam("resourceGroup") String resourceGroup);
 
-	/**
-	 * The Virtual Machine Scale Set API includes operations for managing the
-	 * virtual machines in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163630.aspx">docs</a>
-	 */
-	@Delegate
-	VirtualMachineScaleSetApi getVirtualMachineScaleSetApi(@PathParam("resourceGroup") String resourceGroup);
+   /**
+    * The Virtual Machine API includes operations for managing the virtual machines in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163630.aspx">docs</a>
+    */
+   @Delegate
+   VirtualMachineApi getVirtualMachineApi(@Nullable @PathParam("resourceGroup") String resourceGroup);
 
-	/**
-	 * This Azure Resource Manager API lists all available virtual machine sizes for
-	 * a subscription in a given region
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt269440.aspx">docs</a>
-	 */
-	@Delegate
-	VMSizeApi getVMSizeApi(@PathParam("location") String location);
+   /**
+    * The Virtual Machine Scale Set API includes operations for managing the virtual machines in your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163630.aspx">docs</a>
+    */
+   @Delegate
+   VirtualMachineScaleSetApi getVirtualMachineScaleSetApi(@PathParam("resourceGroup") String resourceGroup);
 
-	/**
-	 * The Azure Resource Manager API gets all the OS images in your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/compute/virtualmachineimages">docs</a>
-	 */
-	@Delegate
-	OSImageApi getOSImageApi(@PathParam("location") String location);
+   /**
+    * This Azure Resource Manager API lists all available virtual machine sizes for a subscription in a given region
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt269440.aspx">docs</a>
+    */
+   @Delegate
+   VMSizeApi getVMSizeApi(@PathParam("location") String location);
 
-	/**
-	 * The Deployment API allows for the management of Azure Resource Manager
-	 * resources through the use of templates.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/dn790549.aspx">docs</a>
-	 */
-	@Delegate
-	DeploymentApi getDeploymentApi(@PathParam("resourcegroup") String resourceGroup);
+   /**
+    * The Azure Resource Manager API gets all the OS images in your subscription.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/compute/virtualmachineimages">docs</a>
+    */
+   @Delegate
+   OSImageApi getOSImageApi(@PathParam("location") String location);
 
-	/**
-	 * The NetworkSecurityGroup API includes operations for managing network
-	 * security groups within your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163615.aspx">docs</a>
-	 */
-	@Delegate
-	NetworkSecurityGroupApi getNetworkSecurityGroupApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The Deployment API allows for the management of Azure Resource Manager resources through the use of templates.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/dn790549.aspx">docs</a>
+    */
+   @Delegate
+   DeploymentApi getDeploymentApi(@PathParam("resourcegroup") String resourceGroup);
 
-	/**
-	 * The NetworkSecurityRule API includes operations for managing network security
-	 * rules within a network security group.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163580.aspx">docs</a>
-	 */
-	@Delegate
-	NetworkSecurityRuleApi getNetworkSecurityRuleApi(@PathParam("resourcegroup") String resourcegroup,
-			@PathParam("networksecuritygroup") String networksecuritygroup);
+   /**
+    * The NetworkSecurityGroup API includes operations for managing network security groups within your subscription.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163615.aspx">docs</a>
+    */
+   @Delegate
+   NetworkSecurityGroupApi getNetworkSecurityGroupApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
+ 
+   /**
+    * The NetworkSecurityRule API includes operations for managing network security rules within a network security group.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/mt163580.aspx">docs</a>
+    */
+   @Delegate
+   NetworkSecurityRuleApi getNetworkSecurityRuleApi(@PathParam("resourcegroup") String resourcegroup,
+                                                    @PathParam("networksecuritygroup") String networksecuritygroup);
 
-	/**
-	 * The LoadBalancer API includes operations for managing load balancers within
-	 * your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/mt163574.aspx">docs </a>
-	 */
-	@Delegate
-	LoadBalancerApi getLoadBalancerApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The LoadBalancer API includes operations for managing load balancers
+    * within your subscription.
+    *
+    * @see <a href=
+    *      "https://msdn.microsoft.com/en-us/library/azure/mt163574.aspx">docs
+    *      </a>
+    */
+   @Delegate
+   LoadBalancerApi getLoadBalancerApi(@Nullable @PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The AvailabilitySet API includes operations for managing availability sets
-	 * within your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/compute/availabilitysets">docs
-	 *      </a>
-	 */
-	@Delegate
-	AvailabilitySetApi getAvailabilitySetApi(@PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The AvailabilitySet API includes operations for managing availability sets
+    * within your subscription.
+    *
+    * @see <a href=
+    *      "https://docs.microsoft.com/en-us/rest/api/compute/availabilitysets">docs
+    *      </a>
+    */
+   @Delegate
+   AvailabilitySetApi getAvailabilitySetApi(@PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The Azure Resource Provider API provides information about a resource
-	 * provider and its supported resource types.
-	 *
-	 * @see <a href=
-	 *      "https://msdn.microsoft.com/en-us/library/azure/dn790534.aspx">docs</a>
-	 */
-	@Delegate
-	ResourceProviderApi getResourceProviderApi();
+   /**
+    * The Azure Resource Provider API provides information about a resource provider and its supported resource types.
+    *
+    * @see <a href="https://msdn.microsoft.com/en-us/library/azure/dn790534.aspx">docs</a>
+    */
+   @Delegate
+   ResourceProviderApi getResourceProviderApi();
 
-	/**
-	 * The ManagedDataDisk API includes operations for managing data disks within
-	 * your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/manageddisks/disks/disks-rest-api">docs</a>
-	 */
-	@Delegate
-	DiskApi getDiskApi(@PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The ManagedDataDisk API includes operations for managing data disks within your subscription.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/manageddisks/disks/disks-rest-api">docs</a>
+    */
+   @Delegate
+   DiskApi getDiskApi(@PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The virtual machine image API includes operations for managing data disks
-	 * within your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/manageddisks/images/images-rest-api">docs</a>
-	 */
-	@Delegate
-	ImageApi getVirtualMachineImageApi(@PathParam("resourcegroup") String resourcegroup);
+   /**
+    * The virtual machine image API includes operations for managing data disks within your subscription.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/manageddisks/images/images-rest-api">docs</a>
+    */
+   @Delegate
+   ImageApi getVirtualMachineImageApi(@PathParam("resourcegroup") String resourcegroup);
 
-	/**
-	 * The metrics API includes operations to get insights into entities within your
-	 * subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/monitor/metrics">docs</a>
-	 */
-	@Delegate
-	MetricsApi getMetricsApi(@PathParam("resourceid") String resourceid);
+   /**
+    * The metrics API includes operations to get insights into entities within your
+    * subscription.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/monitor/metrics">docs</a>
+    */
+   @Delegate
+   MetricsApi getMetricsApi(@PathParam("resourceid") String resourceid);
 
-	/**
-	 * The metric definitions API includes operations to get insights available for
-	 * entities within your subscription.
-	 *
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/monitor/metricdefinitions">docs</a>
-	 */
-	@Delegate
-	MetricDefinitionsApi getMetricsDefinitionsApi(@PathParam("resourceid") String resourceid);
+   /**
+    * The metric definitions API includes operations to get insights available for entities within your
+    * subscription.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/monitor/metricdefinitions">docs</a>
+    */
+   @Delegate
+   MetricDefinitionsApi getMetricsDefinitionsApi(@PathParam("resourceid") String resourceid);
 
-	/**
-	 * The Azure Active Directory Graph API provides programmatic access to Azure AD
-	 * through REST API endpoints.
-	 *
-	 * @see <a href="https://docs.microsoft.com/en-us/rest/api/graphrbac/">docs</a>
-	 */
-	@Delegate
-	GraphRBACApi getGraphRBACApi();
-
-	/**
-	 * Managing your key vaults as well as the keys, secrets, and certificates
-	 * within your key vaults can be accomplished through a REST interface.
-	 *
-	 * @see <a href="https://docs.microsoft.com/en-us/rest/api/keyvault/">docs</a>
-	 */
-	@Delegate
-	VaultApi getVaultApi(@PathParam("resourcegroup") String resourcegroup);
-
-	/**
-	 * Management features for Local Network Gateways.
-	 * 
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/network-gateway/localnetworkgateways">docs</a>
-	 */
-	@Delegate
-	LocalNetworkGatewayApi getLocalNetworkGatewayApi(@PathParam("resourcegroup") String resourcegroup);
-
-	/**
-	 * Management features for Virtual Network Gateways.
-	 * 
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/network-gateway/virtualnetworkgateways">docs</a>
-	 */
-	@Delegate
-	VirtualNetworkGatewayApi getVirtualNetworkGatewayApi(@PathParam("resourcegroup") String resourcegroup);
-
-	/**
-	 * Management features for Virtual Network Gateway Connections.
-	 * 
-	 * @see <a href=
-	 *      "https://docs.microsoft.com/en-us/rest/api/network-gateway/virtualnetworkgatewayconnections">docs</a>
-	 */
-	@Delegate
-	VirtualNetworkGatewayConnectionApi getVirtualNetworkGatewayConnectionApi(
-			@PathParam("resourcegroup") String resourcegroup);
-
-	/**
-	 * Returns the information about the current service principal.
-	 */
-	@Provides
-	Supplier<ServicePrincipal> getServicePrincipal();
-
+   /**
+    * The Azure Active Directory Graph API provides programmatic access to Azure
+    * AD through REST API endpoints.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/graphrbac/">docs</a>
+    */
+   @Delegate
+   GraphRBACApi getGraphRBACApi();
+   
+   /**
+    * Managing your key vaults as well as the keys, secrets, and certificates within your key vaults can be 
+    * accomplished through a REST interface.
+    *
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/keyvault/">docs</a>
+    */
+   @Delegate
+   VaultApi getVaultApi(@PathParam("resourcegroup") String resourcegroup);
+   
+   /**
+    * Management features for Local Network Gateways.
+    * 
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/network-gateway/localnetworkgateways">docs</a>
+    */
+   @Delegate
+   LocalNetworkGatewayApi getLocalNetworkGatewayApi(@PathParam("resourcegroup") String resourcegroup);
+   
+   /**
+    * Management features for Virtual Network Gateways.
+    * 
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/network-gateway/virtualnetworkgateways">docs</a>
+    */
+   @Delegate
+   VirtualNetworkGatewayApi getVirtualNetworkGatewayApi(@PathParam("resourcegroup") String resourcegroup);
+   
+   /**
+    * Management features for Virtual Network Gateway Connections.
+    * 
+    * @see <a href="https://docs.microsoft.com/en-us/rest/api/network-gateway/virtualnetworkgatewayconnections">docs</a>
+    */
+   @Delegate
+   VirtualNetworkGatewayConnectionApi getVirtualNetworkGatewayConnectionApi(@PathParam("resourcegroup") String resourcegroup);
+   
+   /**
+    * Returns the information about the current service principal.
+    */
+   @Provides
+   Supplier<ServicePrincipal> getServicePrincipal();
+   
 	/**
 	 * The Activity Log Alert API includes operations to get insights available for
 	 * entities within your subscription
